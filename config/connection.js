050b3041dd16f2af1a37e9c6c42e776d6d8bcb03
viviann224 <viviann224@gmail.com>
2018-03-02 14:32:42 -0600

    update with jaw

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 //setting up MySQL connection
 var mysql = require("mysql");
-
+/*
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -8,6 +8,22 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
+*/
+var connection ;
+
+if(process.env.JAWSDB_URL)
+{
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+    connection = mysql.createConnection({
+        host : "localhost",
+        user : "root",
+        password : "root",
+        database : "burgers_db"
+    });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
