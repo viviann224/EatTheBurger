e4f3606e599e76bd1b50a7cb2da66c38cb9cb186
viviann224 <viviann224@gmail.com>
2018-03-02 22:10:47 -0600

    updated burgers and server.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,70 +1,59 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
+//this is the logic to change the eaten state
+$(function() 
+{
   $(".change-eaten").on("click", function(event) 
-  {
+  { //only letting the user use one click
+    //once clicked change the state of the button
     var id = $(this).data("id");
     var newdevoured = $(this).data("neweaten");
-    if(newdevoured)
-    {
+    if(newdevoured) //if this is the first click then the 
+    {               //burger is eaten and update the state
       var newDevoured = 
       {
         devoured: newdevoured
       };
-
-      // Send the PUT request.
+      //update the state of the burger to the database
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newDevoured
       }).then(
-        function() {
-          console.log("changed sleep to", newDevoured);
+        function() 
+        {
           // Reload the page to get the updated list
           location.reload();
         }
       );
     }
-    else
-    {
+    else  //else the state is already eaten and remove 
+    {     //button attribute
       $(".change-eaten").RemoveAttr("class","change-eaten");
       $(".change-eaten").attr("id", "eaten");
     }
   });
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-    console.log($("#ca").val().trim());
-    console.log($("[name=devoured]:checked").val());
-    var newBurger = {
-      burger_name: $("#ca").val().trim(),
-      devoured: $("[name=devoured]:checked").val()
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-/*
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE",
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });*/
+//this is the functionality for creating a new burger
+$(".create-form").on("submit", function(event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+  //grabs the burger name
+  console.log($("#ca").val().trim());
+  //grabs the state (not eaten and defaults to not eaten)
+  console.log($("[name=devoured]:checked").val());
+  //pass in the values and return an obj newBurger
+  var newBurger = 
+  {
+    burger_name: $("#ca").val().trim(),
+    devoured: $("[name=devoured]:checked").val()
+  };
+  // Send the POST request. and update to database
+  $.ajax("/api/burgers", {
+    type: "POST",
+    data: newBurger
+  }).then(
+    function() {
+      // Reload the page to get the updated list
+      location.reload();
+    }
+  );
+});
 });
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//express
+//import express
 var express = require("express");
+//import bodyParser
 var bodyParser = require("body-parser");
-
+//create a port 
 var PORT = process.env.PORT || 3000;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -11,13 +11,11 @@ app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -25,7 +23,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
-
+//verify connected
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
